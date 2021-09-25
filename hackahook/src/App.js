eca321539720ae053debd4b26a05e1aa37722f72
Josue Alonso <josueemanuelalonso@gmail.com>
2021-09-25 15:58:17 -0300

    feat: Routes Added

--- hackahook/src/App.js
+++ hackahook/src/App.js
@@ -1,15 +1,53 @@
 import './App.css';
 
+import Login from './pages/Login';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+const Componente1 = () => (
+    <span>Ruta 1</span>
+)
+
+const Componente2 = () => (
+    <span>Ruta 2</span>
+)
+
+const protectedPath = (Component) => {
+    // if(loading)
+    // return <Spinner />
+
+    // if(!logged)
+    // return <Redirect to="/signin"/>
+
+    return <Component/>
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+    return(
+        <Router> 
+            <Switch>
+                <Route exact path="/"
+                    render={Componente1}/>
+
+                <Route path="/home"
+                    render={Componente2}/>
+
+                <Route path="/login"
+                    render={Login}/>
+                <Route path="/register"
+                    render={Componente2}/>
+
+                <Route path="/mybootcamp"
+                    render={Componente2}/>
+                <Route path="/subscriptions"
+                    render={Componente2}/>
+
+                <Route path="*">
+                    <span>404 - Not Found</span>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ hackahook/src/pages/login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div>
+            hola
+        </div>
+    )
+}
+
+export default Login;
