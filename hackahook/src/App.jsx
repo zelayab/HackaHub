e869909274e8f4624e997e0886de472766b7f453
Josue Alonso <josueemanuelalonso@gmail.com>
2021-09-25 17:55:23 -0300

    feat: Added emotion react package on package.json

--- hackahook/src/App.jsx
+++ hackahook/src/App.jsx
@@ -1,46 +1,29 @@
+import React from 'react';
 import './App.css';
 
 import Login from './pages/Login';
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-const Componente1 = () => (
-    <span>Ruta 1</span>
-)
-
-const Componente2 = () => (
-    <span>Ruta 2</span>
-)
-
-const protectedPath = (Component) => {
-    // if(loading)
-    // return <Spinner />
-
-    // if(!logged)
-    // return <Redirect to="/signin"/>
-
-    return <Component/>
-}
-
 function App() {
     return(
         <Router> 
             <Switch>
                 <Route exact path="/"
-                    render={Componente1}/>
+                    render={() => <span></span>}/>
 
                 <Route path="/home"
-                    render={Componente2}/>
+                    render={() => <span></span>}/>
 
                 <Route path="/login"
                     render={Login}/>
                 <Route path="/register"
-                    render={Componente2}/>
+                    render={() => <span></span>}/>
 
                 <Route path="/mybootcamp"
-                    render={Componente2}/>
+                    render={() => <span></span>}/>
                 <Route path="/subscriptions"
-                    render={Componente2}/>
+                    render={() => <span></span>}/>
 
                 <Route path="*">
                     <span>404 - Not Found</span>
